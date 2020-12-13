c6479c399838b0a9fc20458a3efe366136d2cd63
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-12-13 11:38:58 -0700

    state and fetched data showing

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,10 +3,21 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+  constructor() {
+    super();
+    this.state = {
+      plants: []
+    }
+  }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+  componentDidMount() {
+    fetch("http://localhost:3333/plants")
+      .then(res => res.json())
+      .then(res => this.setState({ plants: res.plantsData }))
+      .then(err => console.log(err, "error with fetched data"))
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
