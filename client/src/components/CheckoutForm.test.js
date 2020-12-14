a8f70a9a10d6ecbe50881d25a96c563a662bb174
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-12-14 10:14:29 -0700

    updated second test wanting to pull state values as target still not passing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import useForm from '../hooks/useForm';
 
 // Write up the two tests here and make sure they are testing what the title shows
 
@@ -13,6 +14,11 @@ test("form header renders", () => {
 });
 
 test("form shows success message on submit with form details", () => {
-    render(<CheckoutForm />)
+    const { getByText } = render(<CheckoutForm />)
+
+    const submit = getByText("Checkout Form")
+
+    fireEvent.change(submit, { target: { value: useForm.values } })
+    fireEvent.click(getByText("Submit"))
 
 });
